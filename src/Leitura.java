701bfa0a8f5948bc5515985bff572de96d8b8447
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-02-10 12:08:14 -0300

    video 3.2

---
+++ src/Leitura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Diga sua avaliação para o filme  ");
+        double avaliacao = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+    }
+}
