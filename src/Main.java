2a14be776468d36633479fb8ca2167a08bd75418
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2023-02-10 11:53:50 -0300

    video 2.3

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,17 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
+
     }
 }
